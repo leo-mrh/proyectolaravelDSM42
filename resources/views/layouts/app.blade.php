ccac0e7bfb31c4abf9edc413b85fc214586c40e3
leo-mrh <al222110783@gmail.com>
2023-10-17 18:30:47 -0600

    Login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,10 +16,24 @@
                     DevStagram
                 </h1>
 
-                <nav class="flex gap-2 items-center">
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="">Login</a>
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('register') }}">Crear Cuenta</a>
-                </nav>
+                @auth
+                    <nav class="flex gap-2 items-center">
+                        <a class="font-bold text-gray-600 text-sm" href="#">Hola: <span class="font-normal">{{ auth()->user()->name}}</span></a>
+
+                        <form method="POST" action="{{ route('logout')}}">
+                            @csrf
+                            <button type="submit" font-bold uppercase text-gray-600 text-sm">Cerrar Sesión</button>
+                        </form>
+                    </nav>
+                @endauth
+
+                @guest
+                    <nav class="flex gap-2 items-center">
+                        <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('login') }}">Login</a>
+                        <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('register') }}">Crear Cuenta</a>
+                    </nav>
+                @endguest
+           
             </div>
         </header>
 

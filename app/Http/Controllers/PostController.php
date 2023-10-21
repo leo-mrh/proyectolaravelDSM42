fa1fe93a204a77c7975c66b43290fd74a46668b5
leo-mrh <al222110783@gmail.com>
2023-10-21 08:37:05 -0600

    llegue tarde xD y no entre a clase

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -10,9 +11,12 @@ class PostController extends Controller
     {
         $this->middleware('auth');
     }
-    public function index()
-    {
-       return view('dashboard');
+    public function index(User $user)
+    {   
+      
+       return view('dashboard', [
+        
+       ]);
     }
 
 }
